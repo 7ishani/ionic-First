1198e7cc660881853e5635bf29201d1d0d006a86
7ishani <ishanidilu@gmail.com>
2020-04-16 23:41:05 +0530

    First page implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  user = {
+    name: 'Your Details',
+    
+  };
+  
+
+  constructor(private router: Router, private dataService: DataService) { }
+ 
+  submit() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user,
+    
+
+      }
+    };
+    this.router.navigate(['details'], navigationExtras);
+    
+  }
 
 }
