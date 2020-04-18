884abd5f0a1cb9b7d4f15c002a8ba21de73fa76e
7ishani <ishanidilu@gmail.com>
2020-04-18 15:54:08 +0530

    Display user input in the next page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomePage } from './home/home.page';
+import { DetailsPage } from './details/details.page';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { DataService } from '../services/data.service';
+import { NavController } from '@ionic/angular';
+import { DetailsPage } from '../details/details.page';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  user = {
-    name: 'Your Details',
-    
-  };
+export class HomePage  {
   
-
-  constructor(private router: Router, private dataService: DataService) { }
- 
+  private username:string;
+  
+  constructor(private router: Router, private dataService: DataService) {
+    
+   }
+  
   submit() {
+    console.log("msg : "+this.username);
     let navigationExtras: NavigationExtras = {
       state: {
-        user: this.user,
-    
-
+        user: this.username 
       }
     };
+
     this.router.navigate(['details'], navigationExtras);
-    
+   
   }
 
 }
